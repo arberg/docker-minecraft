aaa9abd279963214d07506460ca9113eededed29
Alex Berg <arberg@gmail.com>
2019-03-24 17:45:07 +0100

    forge version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # docker-minecraft
 PC Java Minecraft docker images
+
+(My) Scripts for creating a docker minecraft server based on https://hub.docker.com/r/itzg/minecraft-server/ docker images
--- ftbRevelation.sh
+++ ftbRevelation.sh
@@ -7,8 +7,8 @@ HOSTDATA=/mnt/user/dockerhub/itzg-minecraft-server/dataFtbRevelation-v1.12
 SERVER_MESSAGE="FTB Revelation 1.12"
 PORT=25565
 MINECRAFT_VERSION=1.12.2
+#FORGE_VERSION=14.23.5.2823
 FORGE_VERSION=14.23.5.2772
-# FORGE_VERSION=14.23.4.2767
 # Login minecraft 
 ONLINE_MODE=false
 # WARNING: With the FtbRevelation modpack, it is necessary to let the docker recreate all config files, in order to change seed, or it will give error about bad config when connecting client to server
